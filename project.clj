e0e4d0005cec3c593630456adc757b56e5cfd58f
Steen Larsen <sl@skm.dk>
2018-03-01 15:23:37 +0100

    cp yaml to s3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aggregate-sag-lambda "0.1.0-SNAPSHOT"
+(defproject aggregate-sag-lambda "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
