7890e569af405b2bf1576b2c0f5c738c46be474c
Steen Larsen <sl@skm.dk>
2018-03-07 17:15:10 +0100

    updated version 11

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aggregate-sag-lambda "0.4.1"
+(defproject aggregate-sag-lambda "0.3.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
